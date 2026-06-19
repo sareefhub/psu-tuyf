d3429ba8991a966c0cc89d4ab9701dab0a1e2aa9
Sareef Masamaeng <sareef.masamaeng@gmail.com>
2026-06-19 10:02:48 +0700

    feat: add student improvement excellence math camp page for specific years

--- src/app/(pages)/mscd/student-improvement/excellence-math-camp/[year]/page.tsx
+++ src/app/(pages)/mscd/student-improvement/excellence-math-camp/[year]/page.tsx
@@ -1,5 +1,5 @@
 import { CampTemplate } from "../components/camp-template"
-import { excellenceCampsData } from "@/data/pages/excellence-camp"
+import { excellenceCampsData } from "@/data/pages/mscd/excellence-camp"
 import { notFound } from "next/navigation"
 
 // ฟังก์ชันระบุค่า Parameter สำหรับการสร้างหน้าเพจแบบ Static (Static Generation Optimization)
