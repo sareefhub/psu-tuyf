fe5e04e1a5da9a6b51634a7a7968aea236a7d4b9
Sareef Masamaeng <sareef.masamaeng@gmail.com>
2026-06-23 10:48:09 +0700

    feat: add student profile for Saowapak Makphon to MGSS data

--- src/data/pages/mgss/students/saowapak-makphon.ts
+++ src/data/pages/mgss/students/saowapak-makphon.ts
@@ -15,5 +15,52 @@ export const saowapakMakphonProfile: MgssStudentProfile = {
     th: "วิทยาเขตหาดใหญ่",
     en: "Hat Yai Campus",
   },
-  activities: [],
+  studyPlan: {
+    th: "หลักสูตรปริญญาโท (ระยะเวลาหลักสูตร 2 ปี) / ดำเนินการศึกษาแล้ว: 1 ปี และ 2 เดือน",
+    en: "Milestone of Graduation: Master degree (Program Time: 2 Years) / Study Progression: 1 Years and 2 Months",
+  },
+  projectTitle: {
+    th: "Solving Riccati fractional differential equations by hybrid of block pulse functions and Mott polynomials",
+    en: "Solving Riccati fractional differential equations by hybrid of block pulse functions and Mott polynomials",
+  },
+  projectAdviser: {
+    th: "Boonrod Yuttanan",
+    en: "Boonrod Yuttanan",
+  },
+  educationalStatus: {
+    th: "อยู่ระหว่างการเขียนเอกสารวิชาการเพื่อตีพิมพ์และเตรียมสอบหัวข้อวิทยานิพนธ์",
+    en: "Writing a paper for publication and prepare for a proposal examination",
+  },
+  activities: [
+    {
+      title: {
+        th: "การบรรยายพิเศษในหัวข้อ \"Introduction to Mathematical Research and My Experience\" โดย รศ.ดร.ประพันธ์พงศ์",
+        en: "Special lecture, titled \"Introduction to Mathematical Research and My Experience\" by Assoc. Prof. Dr. Prapanpong",
+      },
+      date: {
+        th: "",
+        en: "",
+      },
+      description: {
+        th: "",
+        en: "",
+      },
+      images: [],
+    },
+    {
+      title: {
+        th: "โครงการแลกเปลี่ยนความรู้ทางวิทยาศาสตร์ Sakura Science Program ณ มหาวิทยาลัยแห่งชาติโยโกฮามา",
+        en: "Sakura Science Program in Yokohama National University",
+      },
+      date: {
+        th: "",
+        en: "",
+      },
+      description: {
+        th: "",
+        en: "",
+      },
+      images: [],
+    },
+  ],
 }
